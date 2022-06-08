cd88cf693d990c5ead7a304c50e28f93f7d4350a
mbilal <mbilal@bayt.net>
2022-06-09 01:07:30 +0500

    add chain matic testnet

--- src/app/utils/constants.js
+++ src/app/utils/constants.js
@@ -10,8 +10,13 @@ export const collectionStatus = {
 export  const contract_map = {
     4: {
         address:"0x742827F9a7c05F6c2a5D683c97a0b44010519ACB",
-        name : "Rinkebay",
+        name : "Rinkeby",
         unit: "ETH"
+        },
+    80001: {
+            address: '0x425Ed153Be3Fde8973f0d11c1F0058724cBc4Df9',
+            name: 'Matic testnet',
+            unit: 'MATIC'
         }
 }
 
