dd016dac4fc227f25f897ae9a0cf5f3c61d23071
mbilal <mbilal@bayt.net>
2022-06-22 16:49:04 +0500

    remove consoles

--- src/app/pages/Live-Collection/live-collection.js
+++ src/app/pages/Live-Collection/live-collection.js
@@ -7,107 +7,113 @@ import { Button } from "@mui/material";
 import { ArrowRight } from "@mui/icons-material";
 import ViewCollection1img from "../../../assets/images/live/Etherscan.png";
 import ViewCollection2img from "../../../assets/images/live/Opensea.png";
-import { useParams } from 'react-router-dom';
-import { getCollection, getGeneratedCollection, getSaveContract } from "../../api/core";
+import { useParams } from "react-router-dom";
+import {
+  getCollection,
+  getGeneratedCollection,
+  getSaveContract,
+} from "../../api/core";
 import { updateStateAttr } from "../store/authSlice";
 import { useDispatch, useSelector } from "react-redux";
 import { collectionStatus, contract_map } from "../../utils/constants";
-import { CircularProgress } from '@mui/material';
-import LinearProgress from '@mui/material/LinearProgress';
-import {useWeb3React} from '@web3-react/core'
-import {ethers} from 'ethers';
+import { CircularProgress } from "@mui/material";
+import LinearProgress from "@mui/material/LinearProgress";
+import { useWeb3React } from "@web3-react/core";
+import { ethers } from "ethers";
 import { showMessage } from "../store/messageSlice";
-import CheckCircleIcon from '@mui/icons-material/CheckCircle';
-import ContentCopyIcon from '@mui/icons-material/ContentCopy';
-const abi = require('../../../assets/blockchain/factory_abi.json');
+import CheckCircleIcon from "@mui/icons-material/CheckCircle";
+import ContentCopyIcon from "@mui/icons-material/ContentCopy";
+const abi = require("../../../assets/blockchain/factory_abi.json");
 
 const LiveCollection = () => {
-
-  const {isImageGenerated} = useSelector((state)=> state.auth);
+  const { isImageGenerated } = useSelector((state) => state.auth);
   const params = useParams();
-  const [loading, setLoading] = useState(true)
+  const [loading, setLoading] = useState(true);
   const [collection, setCollection] = useState(null);
   const [copied, setCopied] = useState(false);
   const dispatch = useDispatch();
   const initialize = async () => {
     getSaveContract(params.id)
-    .then((response) => {
-      setCollection(response.data);
-      setLoading(false);
-    })
-    .catch(() => {
-      console.log('Something went wrong while fetching contract!');
-    })
+      .then((response) => {
+        setCollection(response.data);
+        setLoading(false);
+      })
+      .catch(() => {
+        console.log("Something went wrong while fetching contract!");
+      });
     // getCollection(params?.id).then((collectionData) =>{
     //   // console.log('colllection', collectionData);
     //    setCollection(collectionData);
     //     setLoading(false);
-     
+
     // }).catch(err => {
     //    console.error(err);
     // })
- }
+  };
 
- useEffect(() => {
-    if(params?.id){
-       initialize();
-       return () => dispatch(updateStateAttr({attr: "isImageGenerated", data: null}))
-    }    
- },[]);
+  useEffect(() => {
+    if (params?.id) {
+      initialize();
+      return () =>
+        dispatch(updateStateAttr({ attr: "isImageGenerated", data: null }));
+    }
+  }, []);
 
-useEffect(() => {
-      if(isImageGenerated){
-        getSaveContract(params.id)
+  useEffect(() => {
+    if (isImageGenerated) {
+      getSaveContract(params.id)
         .then((response) => {
           setCollection(response.data);
           setLoading(false);
         })
         .catch(() => {
-          console.log('Something went wrong while fetching contract!');
-        })
-      }
-    },[isImageGenerated])
+          console.log("Something went wrong while fetching contract!");
+        });
+    }
+  }, [isImageGenerated]);
 
-    function decodeHtml(html) {
-      var txt = document.createElement("textarea");
-      txt.innerHTML = html;
-      return txt.value;
+  function decodeHtml(html) {
+    var txt = document.createElement("textarea");
+    txt.innerHTML = html;
+    return txt.value;
   }
   const copyToClipboard = () => {
-
-    var  buttonSource = document.getElementById("mint-button-code");
-    if(buttonSource){
+    var buttonSource = document.getElementById("mint-button-code");
+    if (buttonSource) {
       const cb = navigator.clipboard;
-      cb.writeText( decodeHtml(buttonSource.innerHTML));
+      cb.writeText(decodeHtml(buttonSource.innerHTML));
       setCopied(true);
     }
-
-  }
+  };
   const renderContent = () => {
-    if(loading) {
-      return(
+    if (loading) {
+      return (
         <section>
-        <div class="max-w-screen-2xl w-11/12 mx-auto mb-16">
-          <div class="p-10 shadow-lg rounded-lg text-center">
-          <CircularProgress />
-          <p class="m-2">Fetching Collection status</p>
+          <div class="max-w-screen-2xl w-11/12 mx-auto mb-16">
+            <div class="p-10 shadow-lg rounded-lg text-center">
+              <CircularProgress />
+              <p class="m-2">Fetching Collection status</p>
+            </div>
           </div>
-        </div>
-      </section>
-      )
+        </section>
+      );
     }
     return (
       <>
-      <section>
-      <div class="max-w-screen-2xl w-11/12 mx-auto">
-        <h2 class="text-2xl font-medium my-8">Embed in Your Website</h2>
-        <div class="flex lg:flex-row flex-col p-10 shadow-lg rounded-lg">
-        
-          <div class="md:ml-5">
-            <h4>Past the Flume script in the {'<head>'} of every page you wish to enable minting button and use <strong>onclick="javascript:mint()"</strong> funtion in your cusomized minting button </h4>
-            <div class="bg-gray-100 p-10 my-4 break-words">
-              <p id="mint-button-code">
-               {`
+        <section>
+          <div class="max-w-screen-2xl w-11/12 mx-auto">
+            <h2 class="text-2xl font-medium my-8">Embed in Your Website</h2>
+            <div class="flex lg:flex-row flex-col p-10 shadow-lg rounded-lg">
+              <div class="md:ml-5">
+                <h4>
+                  Past the Flume script in the {"<head>"} of every page you wish
+                  to enable minting button and use{" "}
+                  <strong>onclick="javascript:mint()"</strong> funtion in your
+                  cusomized minting button{" "}
+                </h4>
+                <div class="bg-gray-100 p-10 my-4 break-words">
+                  <p id="mint-button-code">
+                    {`
                    <script src="https://cdn.ethers.io/lib/ethers-5.2.umd.min.js"
                    type="application/javascript"></script>
                    <script >
@@ -153,83 +159,103 @@ useEffect(() => {
                      
                    </script>
                `}
-              </p>
+                  </p>
+                </div>
+                <div class="flex flex-col justify-between min-w-fit md:mr-5">
+                  <div>
+                    <Button
+                      startIcon={<ContentCopyIcon />}
+                      sx={{ color: "white" }}
+                      color="secondary"
+                      variant="contained"
+                      onClick={() => copyToClipboard()}
+                    >
+                      {copied ? "Copied Snippet" : "Copy Snippet"}
+                    </Button>
+                  </div>
+                </div>
+              </div>
             </div>
-            <div class="flex flex-col justify-between min-w-fit md:mr-5">
-
-<div>
-  <Button startIcon={<ContentCopyIcon />} sx={{color: 'white'}}  color="secondary" variant="contained"  onClick={()=> copyToClipboard()}>
-    {copied? 'Copied Snippet': 'Copy Snippet'} 
-  </Button>
-</div>
-</div>
           </div>
-        </div>
-      </div>
-    </section>
-    
-    <section>
-      <div class="max-w-screen-2xl w-11/12 mx-auto">
-        <h2 class="text-2xl font-medium my-8">External Links</h2>
-        <div class="flex md:flex-row flex-col p-10 shadow-lg rounded-lg justify-between">
-          <div class="flex justify-between flex-grow">
-            <a href={`https://rinkeby.etherscan.io/address/${collection.contract_address}`} target="_blank" rel="noreferrer">
-            <button
-              size="small"
-              class="p-2 m-2 flex-grow flex border-1 rounded-lg border-gray bg-gray justify-evenly items-center"
-            >
-              <img src={ViewCollection1img} class="px-2"></img>{" "}
-              <span class="px-2">View on Etherscan</span> <ArrowRight />
-            </button>
-            </a>
-            <a href="https://testnets.opensea.io/get-listed" target="_blank" rel="noreferrer">
-            <button
-              size="small"
-              class="p-2 m-2 flex-grow flex border-1 rounded-lg border-gray bg-gray justify-evenly items-center"
-            >
-              <img src={ViewCollection2img} class="px-2"></img>{" "}
-              <span class="px-2">View on Opensea</span> <ArrowRight />
-            </button>
-            </a>
-            <a href={`https://rinkeby.etherscan.io/address/${collection.contract_address}#code`} target="_blank" rel="noreferrer">
-            <button
-              size="small"
-              class="p-2 m-2 flex-grow flex border-1 rounded-lg border-gray bg-gray justify-evenly items-center"
-            >
-              <span class="px-2">Smart Contract Source Code</span> <ArrowRight />
-            </button>
-            </a>
-          </div>
-        </div>
-      </div>
-    </section>
-    {collection?.status === collectionStatus.PINNING ? (
-        <section>
-        <div class="max-w-screen-2xl w-11/12 mx-auto mb-16">
-          <h2 class="text-2xl font-medium my-8">Files synchronization with IPFS</h2>
-          <div class="p-10 shadow-lg rounded-lg">
-          <p class="m-2 text-lg">Please wait Files are synchronizing</p>
-          <LinearProgress />
-          </div>
-        </div>
         </section>
-    ):
-    (
-      <section>
-      <div class="max-w-screen-2xl w-11/12 mx-auto mb-16">
-      <h2 class="text-2xl font-medium my-8">Files synchronization with IPFS</h2>
-      <div class="p-10 shadow-lg rounded-lg">
-          <p class="m-2 text-lg">Files are synchronized <CheckCircleIcon color="primary" /></p>
-          </div>
-      </div>
-      </section>
-    )
-    }
-   
-    </>
-    )
 
-  }
+        <section>
+          <div class="max-w-screen-2xl w-11/12 mx-auto">
+            <h2 class="text-2xl font-medium my-8">External Links</h2>
+            <div class="flex md:flex-row flex-col p-10 shadow-lg rounded-lg justify-between">
+              <div class="flex justify-between flex-grow">
+                <a
+                  href={`https://rinkeby.etherscan.io/address/${collection.contract_address}`}
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <button
+                    size="small"
+                    class="p-2 m-2 flex-grow flex border-1 rounded-lg border-gray bg-gray justify-evenly items-center"
+                  >
+                    <img src={ViewCollection1img} class="px-2"></img>{" "}
+                    <span class="px-2">View on Etherscan</span> <ArrowRight />
+                  </button>
+                </a>
+                <a
+                  href="https://testnets.opensea.io/get-listed"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <button
+                    size="small"
+                    class="p-2 m-2 flex-grow flex border-1 rounded-lg border-gray bg-gray justify-evenly items-center"
+                  >
+                    <img src={ViewCollection2img} class="px-2"></img>{" "}
+                    <span class="px-2">View on Opensea</span> <ArrowRight />
+                  </button>
+                </a>
+                <a
+                  href={`https://rinkeby.etherscan.io/address/${collection.contract_address}#code`}
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <button
+                    size="small"
+                    class="p-2 m-2 flex-grow flex border-1 rounded-lg border-gray bg-gray justify-evenly items-center"
+                  >
+                    <span class="px-2">Smart Contract Source Code</span>{" "}
+                    <ArrowRight />
+                  </button>
+                </a>
+              </div>
+            </div>
+          </div>
+        </section>
+        {collection?.status === collectionStatus.PINNING ? (
+          <section>
+            <div class="max-w-screen-2xl w-11/12 mx-auto mb-16">
+              <h2 class="text-2xl font-medium my-8">
+                Files synchronization with IPFS
+              </h2>
+              <div class="p-10 shadow-lg rounded-lg">
+                <p class="m-2 text-lg">Please wait Files are synchronizing</p>
+                <LinearProgress />
+              </div>
+            </div>
+          </section>
+        ) : (
+          <section>
+            <div class="max-w-screen-2xl w-11/12 mx-auto mb-16">
+              <h2 class="text-2xl font-medium my-8">
+                Files synchronization with IPFS
+              </h2>
+              <div class="p-10 shadow-lg rounded-lg">
+                <p class="m-2 text-lg">
+                  Files are synchronized <CheckCircleIcon color="primary" />
+                </p>
+              </div>
+            </div>
+          </section>
+        )}
+      </>
+    );
+  };
   return (
     <div>
       <Header />
@@ -237,15 +263,18 @@ useEffect(() => {
         <div class="max-w-screen-2xl w-11/12 mx-auto flex">
           <div class="my-5 sm:my-10 pr-0 sm:pr-6 text-left self-center">
             <h1 class="text-third xl:text-6xl lg:text-5xl md:text-4xl text-3xl mb-3">
-            <p> Collection is <span class="text-secondary">Live</span> </p>
-                 <p class="text-third sm:text-2xl text-xl mb-3">
-              Transaction Confirmed
-            </p>
+              <p>
+                {" "}
+                Collection is <span class="text-secondary">Live</span>{" "}
+              </p>
+              <p class="text-third sm:text-2xl text-xl mb-3">
+                Transaction Confirmed
+              </p>
             </h1>
           </div>
         </div>
       </section>
-     {renderContent()}
+      {renderContent()}
     </div>
   );
 };
